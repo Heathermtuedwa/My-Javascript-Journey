0ea8226e05010f2b74369462a40b807665687adb
heathermtuedwa <heathermtuedwa@gmail.com>
2023-11-08 12:14:28 +0200

    Added Files

--- index.js
+++ index.js
@@ -11,7 +11,43 @@ let num2 = 4;
 let remainder = length * width ;
 console.log(`The remind is ${remainder}`)
 
-let num1 = 10;
-let num2 = 4;
-let remainder = num1 % num2;
+let num5 = 10;
+let num7 = 4;
+let division = num1 % num2;
 console.log(`num1/ num2 = ${divison}`)
+
+//Task2 
+
+let integerNumber = 25 ;
+
+let decimalNumber = 25.5;
+
+let myString = "My Name is Heather!";
+
+let myObject = {
+    name: "Heather",
+    age: 25,
+    City: "San Francisco",
+    Country: "South Africa",
+}
+let myArry = [25,Heather,City ]
+
+console.log(`myArry = ${myArry}`)
+console.log(`myObject = ${MyObject}`)
+console.log(`myString = ${myString}`)
+console.log(`decimalNumber = ${decimalNumber}`)
+console.log(`integerNumber = ${integerNumber}`)
+
+console.log(typeof myArry)
+console.log(typeof myObject)
+console.log(typeof myString)
+console.log(typeof integerNumber)
+console.log(typeof decimalNumber)
+
+let array = ["one",1, "two",2, "three",3]
+console.log(typeof array)
+ 
+
+let nullVariable = "Two";
+let undefinedVariable = "Three";
+
